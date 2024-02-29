2276c42138ecfb8a29736d31e200271f95f0b228
SOURABH YADAV <145215052+thefuturewebix@users.noreply.github.com>
2024-03-01 02:18:57 +0530

    Basics VD1&2

---
+++ 02_DataTypes.js
@@ -0,0 +1,9 @@
+"use strict";  //treat all JS code as newer Version
+
+// alert(3+3); //We are using nodejs ,not Browser
+
+// For Documentation There are two Sites
+// 1} mdn Website 
+// 2} tc39.es //Official Website for Js Standards
+
+
