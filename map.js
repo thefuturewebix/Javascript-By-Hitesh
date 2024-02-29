6d59839b667004d395e3dea509cac6465b5b7f19
SOURABH YADAV <145215052+thefuturewebix@users.noreply.github.com>
2024-02-29 21:07:35 +0530

    Update map.js

--- map.js
+++ map.js
@@ -6,7 +6,10 @@ const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
                 // Here similarly if we open the scopes {} then we need to use return to return the value 
                 
     // ✨Chaining✨
+    // we can apply map&filter function over a map function this is called chaining in Js 
     
+let newNum = myNumber.map((num) => num * 10 ).map((num) => num+1);
+console.log(newNum);
 
-
+newNum = myNumber.map((num) => num * 10 ).map((num) => num+1).filter((num) => num>40);
 console.log(newNum);
