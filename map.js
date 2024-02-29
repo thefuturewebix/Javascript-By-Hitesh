bb186fe10d1c8b3ad148f978942c08c4197e1dd9
SOURABH YADAV <145215052+thefuturewebix@users.noreply.github.com>
2024-02-29 14:09:30 +0000

    Renamed

---
+++ map.js
@@ -0,0 +1,12 @@
+                // ✨map✨
+        // Map Automotically Returns the Value we donot need to use return to return the function value 
+
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // const newNum = myNumber.map( (val) => val+10 )
+                // Here similarly if we open the scopes {} then we need to use return to return the value 
+                
+    // ✨Chaining✨
+    
+
+
+console.log(newNum);
